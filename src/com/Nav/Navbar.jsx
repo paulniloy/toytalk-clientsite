a3f1c2a4fbdc92b7eb66a860ccbe6f9ae627291b
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-17 20:39:13 +0600

    navbar done

--- src/com/Nav/Navbar.jsx
+++ src/com/Nav/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import logo from "../../assets/toysbrand.png"
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            this is the navbar
+        <div className='flex justify-between p-5 bg-red-200'>
+            <div className='flex items-center gap-10'>
+                <img className='w-20' src={logo} alt="" />
+                <h1 className='text-3xl font-bold italic'>Toy Talks</h1>
+            </div>
+            <div className='flex gap-10 items-center'>
+                <Link to={""}>Home</Link>
+                <Link to={""}>All Toys</Link>
+                <Link to={""}>My Toys</Link>
+                <Link to={""}>Add Toys</Link>
+                <Link to={""}>Blogs</Link>
+                <Link to={""}>User profile</Link>
+            </div>
         </div>
     );
 };
