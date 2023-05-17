94fb3bc5017dc1b0df4033908cc4ca7dc2360e47
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-05-18 00:56:32 +0600

    conditional login done

--- src/com/Nav/Navbar.jsx
+++ src/com/Nav/Navbar.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from "../../assets/toysbrand.png"
 import { Link } from 'react-router-dom';
+import { Authcontext } from '../Authprovider/Auth';
 
 const Navbar = () => {
+    const {loggeduser} = useContext(Authcontext);
+    
+
+    const handlelogout = () =>{
+        
+    }
+
+
+    const conditional =
+        <>
+            <Link to={""}>My Toys</Link>
+            <Link to={""}>Add Toys</Link>
+            <Link onClick={handlelogout} to={"/login"}>logout</Link>
+        </>
+
+
     return (
         <div className='flex justify-between p-5 bg-red-200'>
             <div className='flex items-center gap-10'>
@@ -12,10 +29,10 @@ const Navbar = () => {
             <div className='flex gap-10 items-center'>
                 <Link to={""}>Home</Link>
                 <Link to={""}>All Toys</Link>
-                <Link to={""}>My Toys</Link>
-                <Link to={""}>Add Toys</Link>
                 <Link to={"/blog"}>Blogs</Link>
-                <Link to={"/login"}>Login</Link>
+                {
+                    loggeduser? conditional : <Link to={"/login"}>Login</Link> 
+                }
             </div>
         </div>
     );
